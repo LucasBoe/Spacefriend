2a9c73e79ad536bd14b56e570ed56e5bdcf95730
LucasBoe <lucas@lumotex.de>
2022-05-23 09:46:20 +0200

    Added pathfinding for navigation

--- Assets/Scripts/Navigation/NavigationGrid.cs
+++ Assets/Scripts/Navigation/NavigationGrid.cs
@@ -6,22 +6,103 @@ using UnityEngine;
 public class NavigationGrid : MonoBehaviour
 {
     NavigationLink[] links;
+    Vector3[] debugPoints;
 
     private void Awake()
     {
         links = GetComponentsInChildren<NavigationLink>();
     }
 
-    internal List<Vector3>GetPath(Vector3 start, Vector3 target)
+    internal List<Vector3> GetPath(Vector3 start, Vector3 target)
     {
-        List<Vector3> path = new List<Vector3>();
 
         NavigationLink startLink = GetClosestLinkTo(start);
-        NavigationLink endLink = GetClosestLinkTo(target);
+        NavigationLink targetLink = GetClosestLinkTo(target);
 
-        path.Add(endLink.ToLine(target));
+        List<Vector3> path = new List<Vector3>();
+
+        List<NavigationNode> openSet = new List<NavigationNode>();
+        HashSet<NavigationNode> closeSet = new HashSet<NavigationNode>();
+
+        openSet.Add(new NavigationNode() { Link = startLink });
+
+        while (openSet.Count > 0)
+        {
+            NavigationNode current = openSet[0];
+
+            for (int i = 1; i < openSet.Count; i++)
+            {
+                if (openSet[i].FCost < current.FCost || openSet[i].FCost == current.FCost && openSet[i].HCost < current.HCost)
+                    current = openSet[i];
+            }
+
+            openSet.Remove(current);
+            closeSet.Add(current);
+
+            if (current.Link == targetLink)
+            {
+                NavigationPoint previous = current.GetSharedPoint(current.Parent);
+                path.Add(previous.transform.position);
+
+                current = current.Parent;
+                
+                while (current.Link != startLink)
+                {
+                    NavigationPoint next = current.Link.GetOtherPoint(previous);
+
+                    Debug.Log(next.name);
+
+                    path.Add(next.transform.position);
+                    previous = next;
+                    current = current.Parent;
+                }
+
+                path.Reverse();
+
+                path.Add(targetLink.ToLine(target));
+
+                debugPoints = path.ToArray();
+                return path;
+            }
+
+            foreach (NavigationNode neightbour in GetNeightbours(current, links))
+            {
+                if (closeSet.Contains(neightbour)) continue;
+
+                float newCost = current.GCost + neightbour.GetDistance(current.Link);
+                if (newCost < neightbour.GCost || !openSet.Contains(neightbour))
+                {
+                    neightbour.GCost = newCost;
+                    neightbour.HCost = neightbour.GetDistance(targetLink);
+                    neightbour.Parent = current;
+
+                    if (!openSet.Contains(neightbour))
+                        openSet.Add(neightbour);
+                }
+            }
+        }
 
         return path;
+    }
+    private List<NavigationNode> GetNeightbours(NavigationNode node, NavigationLink[] links)
+    {
+        List<NavigationNode> list = new List<NavigationNode>();
+
+        HashSet<NavigationPoint> points = new HashSet<NavigationPoint>(node.Link.Points);
+
+        foreach (NavigationLink link in links)
+        {
+            if (link != node.Link)
+            {
+                foreach (NavigationPoint point in link.Points)
+                {
+                    if (points.Contains(point))
+                        list.Add(new NavigationNode() { Link = link });
+                }
+            }
+        }
+
+        return list;
     }
 
     private NavigationLink GetClosestLinkTo(Vector3 point)
@@ -44,5 +125,43 @@ public class NavigationGrid : MonoBehaviour
         Debug.DrawLine(point, link.ToLine(point));
 
         return link;
+    }
+
+    private class NavigationNode
+    {
+        public NavigationLink Link;
+        public float GCost, HCost;
+        public float FCost => GCost + HCost;
+        public NavigationNode Parent;
+
+        internal float GetDistance(NavigationLink neightbour)
+        {
+            return Vector2.Distance(Vector2.Lerp(Link.Points[0].transform.position, Link.Points[1].transform.position, 0.5f), Vector2.Lerp(neightbour.Points[0].transform.position, neightbour.Points[1].transform.position, 0.5f));
+        }
+
+        internal NavigationPoint GetSharedPoint(NavigationNode parent)
+        {
+            foreach (NavigationPoint point in Link.Points)
+            {
+                foreach (NavigationPoint p in parent.Link.Points)
+                {
+                    if (p == point) return point;
+                }
+            }
+
+            return null;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (debugPoints != null && debugPoints.Length != 0)
+        {
+            for (int i = 1; i < debugPoints.Length - 1; i++)
+            {
+                Gizmos.color = Color.yellow;
+                Gizmos.DrawLine(debugPoints[i- 1], debugPoints[i]);
+            }
+        }
     }
 }
--- Assets/Scripts/Navigation/NavigationLink.cs
+++ Assets/Scripts/Navigation/NavigationLink.cs
@@ -8,7 +8,7 @@ public class NavigationLink : MonoBehaviour
     [SerializeField] NavigationPoint[] points = new NavigationPoint[2];
     public NavigationPoint[] Points => points;
 
-    private void OnDrawGizmos()
+    private void OnDrawGizmosSelected()
     {
         if (points[0] != null && points[1] != null)
         {
@@ -21,5 +21,36 @@ public class NavigationLink : MonoBehaviour
     {
         Vector2 vector2 = Vector2Util.GetClosestPointOnLineSegment(points[0].transform.position, points[1].transform.position, target);
         return new Vector3(vector2.x, vector2.y, 0);
+    }
+
+    internal Vector3 GetNextPoint(Vector3 current, Vector3 target)
+    {
+        float d1 = Vector2.Distance(current, points[0].transform.position);
+        float d2 = Vector2.Distance(current, points[1].transform.position);
+
+        if (d1 < 0.01f)
+            return points[1].transform.position;
+        else if (d2 < 0.01f)
+            return points[0].transform.position;
+        else
+            return ToLine(target);
+
+
+    }
+
+    internal NavigationPoint GetFurthestPoint(Vector3 target)
+    {
+        if (Vector2.Distance(target, points[0].transform.position) < Vector2.Distance(target, points[1].transform.position))
+            return points[1];
+        else
+            return points[0];
+    }
+
+    internal NavigationPoint GetOtherPoint(NavigationPoint previous)
+    {
+        if (points[0] == previous)
+            return points[1];
+        else
+            return points[0];
     }
 }
