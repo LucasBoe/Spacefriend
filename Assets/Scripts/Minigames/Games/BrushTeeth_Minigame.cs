8516fae453ab881ffdd53d1dabb4d201c100b988
LucasBoe <lucas@lumotex.de>
2022-05-28 14:53:51 +0200

    Added brushing theeth minigame

---
+++ Assets/Scripts/Minigames/Games/BrushTeeth_Minigame.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BrushTeeth_Minigame : MinigamePhase
+{
+    [SerializeField] GameObject overriders;
+    [SerializeField] SliderUIBehaviour slider;
+    [SerializeField, PlayerAnimatorParam] string brushTeethParam;
+    [SerializeField] ParticleSystem brushParticles;
+    ParticleSystem.EmissionModule emissionModule;
+    [SerializeField] AnimationCurve brushDeltaToParticlesPerSecondCurve;
+    [SerializeField] AudioSource brushSource;
+    [SerializeField] AnimationCurve brushDeltaToPitch, brushDeltaToVolume;
+
+    Animator playerAnimator;
+    float lastBrushValue = 0f;
+    float newBrushDelta = 0f;
+    float smoothedDelta;
+    bool active = false;
+    bool holdingMouse = false;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        emissionModule = brushParticles.emission;
+    }
+
+    private void Start()
+    {
+        playerAnimator = PlayerManager.GetPlayerAnimator();
+    }
+
+    private void Update()
+    {
+        if (!active) return;
+
+        float brushValueDelta = newBrushDelta;
+        newBrushDelta = 0f;
+
+        SetParticleEmissionBasedOnBrushDelta(brushValueDelta);
+
+        smoothedDelta = Mathf.Lerp(smoothedDelta, brushValueDelta, Time.deltaTime * 10f);
+        brushSource.pitch = brushDeltaToPitch.Evaluate(smoothedDelta);
+        brushSource.volume = brushDeltaToVolume.Evaluate(smoothedDelta);
+    }
+
+    public override void StartPhase()
+    {
+        active = true;
+        base.StartPhase();
+        overriders.SetActive(true);
+        slider.OnValueChanged.AddListener(OnValueChanged);
+        brushSource.volume = 0;
+        brushSource.Play();
+        CoroutineUtil.Delay(EndPhase, this, 10f);
+    }
+
+    public override void EndPhase()
+    {
+        active = false;
+        base.EndPhase();
+        overriders.SetActive(false);
+        SetParticleEmissionBasedOnBrushDelta(0f);
+        slider.OnValueChanged.RemoveListener(OnValueChanged);
+        CoroutineUtil.ExecuteFloatRoutine(brushSource.volume, 0, (float value) => brushSource.volume = value, this, duration: 0.5f);
+    }
+    private void OnValueChanged(float brushValue)
+    {
+        newBrushDelta = Mathf.Abs(brushValue - lastBrushValue);
+        lastBrushValue = brushValue;
+        playerAnimator.SetFloat(brushTeethParam, Mathf.Clamp(brushValue, 0f, 0.999f));
+    }
+
+    private void SetParticleEmissionBasedOnBrushDelta(float brushValueDelta)
+    {
+        emissionModule.rateOverTime = new ParticleSystem.MinMaxCurve(brushDeltaToParticlesPerSecondCurve.Evaluate(brushValueDelta));
+    }
+}
--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -6,13 +6,18 @@ using UnityEngine;
 public class PlayerManager : SingletonBehaviour<PlayerManager>
 {
     Player player;
+    Animator playerAimator;
     protected override void Awake()
     {
         base.Awake();
         player = FindObjectOfType<Player>();
+        playerAimator = player.GetComponentInChildren<Animator>();
     }
 
     public static void SetPositionOverride(PlayerPositionOverrider playerPositionOverrider) => Instance.player.MoveModule.SetPositionOverride(playerPositionOverrider);
+
+    public static Animator GetPlayerAnimator() =>  Instance.playerAimator;
+
     public static void RevokeOverridePosition(PlayerPositionOverrider playerPositionOverrider) => Instance.player.MoveModule.RevokeOverridePosition(playerPositionOverrider);
 
     public static PlayerSkinType GetPlayerSkin() => Instance.player.SkinModule.SkinType;
