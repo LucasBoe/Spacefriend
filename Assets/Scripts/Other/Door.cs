3090f3b3f4205f9c252f4be487bc9726aba34474
LucasBoe <lucas@lumotex.de>
2022-05-28 09:18:03 +0200

    Cleaed up prefab and shader folder and implemented star trek door sound

--- Assets/Scripts/Other/Door.cs
+++ Assets/Scripts/Other/Door.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using SoundSystem;
 
 [RequireComponent(typeof(SpriteRendererMaterialInstanciator))]
 public class Door : MonoBehaviour
 {
     Material material;
+    [SerializeField] Sound doorSound;
     private void Awake()
     {
         material = GetComponent<SpriteRenderer>().material;
@@ -15,11 +17,13 @@ public class Door : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         CoroutineUtil.ExecuteFloatRoutine(0, 1, AnimateDoor, this, 0.5f);
+        doorSound.Play();
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         CoroutineUtil.ExecuteFloatRoutine(1, 0, AnimateDoor, this, 0.5f);
+        doorSound.Play();
     }
 
     private void AnimateDoor(float value)
